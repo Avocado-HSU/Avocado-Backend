b872df752a835e85596e3369fb69c6c43a52e24c
D15M4S <dismas5184@gmail.com>
2024-07-08 03:25:45 +0900

    :bug: healthCheck 관련 에러 수정

--- src/main/java/dismas/com/avocado/controller/HealthCheckAPI.java
+++ src/main/java/dismas/com/avocado/controller/HealthCheckAPI.java
@@ -1,7 +1,9 @@
 package dismas.com.avocado.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class HealthCheckAPI {
 
     @GetMapping("/health")
