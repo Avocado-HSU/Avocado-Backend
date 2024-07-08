595ee709cf9a297c4d23c9252c685c7943e921b8
D15M4S <dismas5184@gmail.com>
2024-07-08 22:44:08 +0900

    feat: 스프링 데이터 Auditing 설정
    BaseEntity를 통해 Auditing 설정
    - 생성일자 저장
    - 수정일자 저장
    해당 내용을 데이터베이스에 저장 후 운영

---
+++ src/main/java/dismas/com/avocado/dto/BaseEntity.java
@@ -0,0 +1,26 @@
+package dismas.com.avocado.dto;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import java.time.LocalDateTime;
+
+/**
+ * 엔티티 생성 일자 및 업데이트 일자 정보를 담는 클래스
+ */
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+public class BaseEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+}
